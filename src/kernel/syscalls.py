ab17ea8c0638fac38809dd10b1ebbf62dd590a30
Websitehosterio <124309800+Websitehosterio@users.noreply.github.com>
2024-01-12 22:52:25 -0500

    Update syscalls.py

--- src/kernel/syscalls.py
+++ src/kernel/syscalls.py
@@ -1,4 +1,9 @@
 from Kernel import *
 syscalls = {
-    "1": read_file,
+    "1": read_file,    
+    "2": write_file,
+    "3": delete_file,
+    '4': read_folder,
+    '5': write_folder,
+    '6': delete_folder,
 }
