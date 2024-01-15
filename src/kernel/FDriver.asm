c99de3d7637a1b6be368325f057176d0dd35d91b
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-01-15 16:38:07 -0500

    Create FDriver.asm

---
+++ src/kernel/FDriver.asm
@@ -0,0 +1,30 @@
+section .text
+ global _FDriver
+ global _SetAllPermissionsToOne
+section .data
+  file_permissions: dd 4
+  can_read_file: dd 0
+  can_write_file: dd 0
+  can_delete_file: dd 0
+  writing_file: dd 0
+  reading_file: dd 0
+  deleting_file: dd 0
+
+  folder_permissions: dd 0
+  can_read_folder: dd 0
+  can_write_folder: dd 0
+  can_delete_folder: dd 0
+  deleting_folder: dd 0
+  writing_folder: dd 0
+  reading_folder: dd 0
+
+_FDriver:
+; Checking file permission settings
+ cmp dword [file_permissions], 4
+ je _SetAllPermissionsToOne
+
+_SetAllPermissionsToOne:
+  mov dword [can_write_file], 1
+  mov dword [can_read_file], 1
+  mov dword [can_delete_file], 1
+  jmp _FDriver
