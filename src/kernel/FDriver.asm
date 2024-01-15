6ac2ffb1046d737cbea470a495e8e8b1961ea1ea
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-01-15 16:48:17 -0500

    Update FDriver.asm

--- src/kernel/FDriver.asm
+++ src/kernel/FDriver.asm
@@ -3,6 +3,7 @@ section .text
  global _SetAllPermissionsToOne
  global _CheckPermissions
 section .data
+  ; File Handling And Permissions
   file_permissions: dd 4
   can_read_file: dd 0
   can_write_file: dd 0
@@ -11,6 +12,7 @@ section .data
   reading_file: dd 0
   deleting_file: dd 0
 
+  ; Folder Permissions And Handling
   folder_permissions: dd 0
   can_read_folder: dd 0
   can_write_folder: dd 0
@@ -20,22 +22,30 @@ section .data
   reading_folder: dd 0
 
 _FDriver:
+
+; File Permissions
 ; Checking file permission settings
+
+ ; Checking if `file_permissions` variable is set to 4 (EG. All)
  cmp dword [file_permissions], 4
  je _SetAllPermissionsToOne
- 
+
 _SetAllPermissionsToOne:
   mov dword [can_write_file], 1
   mov dword [can_read_file], 1
   mov dword [can_delete_file], 1
-  jmp _FDriver
+  
+   jmp _FDriver
 
 _CheckPermissions:
-  %include "Kernel.asm"
+  %include "Kernel.asm" ; Include Kernel (eg. `CheckIfReadingFile`, `CheckIfWritingFile`, `CheckIfDeletingFile`
+  ; and `checkIfDeletingFile`)
 
   cmp dword [can_read_file], 1
   je _CheckIfReadingFile
+  
   cmp dword [can_write_file], 1
   je _CheckIfWritingFile
+  
   cmp dword [can_delete_file], 1
   je _checkIfDeletingFile
