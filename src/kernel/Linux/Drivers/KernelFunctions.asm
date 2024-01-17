3aee316534f244820d47b1e7707cd8a23630a326
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-01-16 22:05:52 -0500

    Update KernelFunctions.asm

--- src/kernel/Linux/Drivers/KernelFunctions.asm
+++ src/kernel/Linux/Drivers/KernelFunctions.asm
@@ -1,1 +1,7 @@
-
+;  This file handles all of the file and folder related tasks.
+section .text
+ global _CheckIfWritingFile   ;  This function will check if a file is being written. 
+ global _WriteFile            ;  This function will write a file
+ global _CheckIfDeletingFile  ;  This function will check if a file has been requested to be deleted
+_CheckIfWritingFile:
+    ; The rest of the code here...
