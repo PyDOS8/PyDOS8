34c92ae5705968bf544c935e6184f1a06cfab7b0
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-01-19 17:49:53 -0500

    Update Macros.asm

--- src/kernel/Linux/Drivers/Macros.asm
+++ src/kernel/Linux/Drivers/Macros.asm
@@ -4,25 +4,25 @@
 
 %macro load1OntoStack 1
  push %1
-endmacro%
+%endmacro
 
 %macro load2OntoStack 2
   push %1
   push %2
-endmacro%
+%endmacro
 
 %macro load3OntoStack 3
   push %1
   push %2
   push %3
-endmacro%
+%endmacro
 
 %macro load4OntoStack 4
   push %1
   push %2
   push %3
   push %4
-endmacro%
+%endmacro
 
 %macro load5OntoStack 5
   push %1
@@ -30,7 +30,7 @@ endmacro%
   push %3
   push %4
   push %5
-endmacro%
+%endmacro
 
 %macro load6OntoStack 6
   push %1
@@ -39,7 +39,7 @@ endmacro%
   push %4
   push %5
   push %6
-endmacro%
+%endmacro
 
 %macro deallocate6FromStack 6
  pop %1
@@ -48,29 +48,29 @@ endmacro%
  pop %4
  pop %5
  pop %6
-endmacro%
+%endmacro
 
 %macro deallocate1FromStack 1
   pop %1
-endmacro%
+%endmacro
 
 %macro deallocate2FromStack 2
   pop %1
   pop %2
-endmacro%
+%endmacro
 
 %macro deallocate3FromStack 3
   pop %1
   pop %2
   pop %3
-endmacro%
+%endmacro
 
 %macro deallocate4FromStack 4  
   pop %1
   pop %2
   pop %3
   pop %4
-endmacro%
+%endmacro
 
 %macro deallocate5FromStack 5
   pop %1
@@ -78,7 +78,7 @@ endmacro%
   pop %3
   pop %4
   pop %5
-endmacro%
+%endmacro
 
 
 ; General functions
@@ -86,79 +86,80 @@ endmacro%
 ; Moving data around
 %macro move 2
   mov %1, %2
-endmacro%
+%endmacro
 
 ; Exiting
 %macro exit 2
   mov eax, %1
   xor %2, %2
-endmacro%
+%endmacro
 
 ; JMP
 %macro goto 1
   jmp _%1
-endmacro%
+%endmacro
 
 ; Comparing
 %macro compare 2
   cmp %1, %2
-endmacro%
+%endmacro
 
 ; JE
 %macro GIFE 1
  je _%1
-endmacro%
+%endmacro
 
 ; JL
 %macro gotoifless 1
   jl _%1
-endmacro%
+%endmacro
 
 ; JG
 %macro GIFG 1
   jg _%1
-endmacro%
+%endmacro
 
 ;JGE
 %macro GIFEL 1
   jge _%1
-endmacro%
+%endmacro
 
 %macro moverl 2
   mov %2, %1
-endmacro%
+%endmacro
 
 ; JGL
 %macro GIFLG 1
   jle _%1
-endmacro%
+%endmacro
 
 ; JLE
 %macro GIFLE 1
   jle _%1
-endmacro%
+%endmacro
 
 ; For loop
 %macro ForLoopJL 3
  cmp %1, %2
  jl _%3
-endmacro%
+%endmacro
 
 ; For loop 2
 %macro ForLoopJE 3
  cmp %1, %2
  je _%3
+%endmacro
 
 ; For loop 3
 %macro ForLoopJG 3
  cmp %1, %2
  jg _%3
-endmacro%
+%endmacro
 
 %macro text %1
  section .text
   global _%1
-endmacro%
+%endmacro
 
 %macro interrupt 1
  int %1
@@ -166,19 +167,19 @@ endmacro%
 
 %macro multiply 2
   mul %1, %2
-endmacro%
+%endmacro
 
 %macro divide 2
   div %1, %2
-endmacro%
+%endmacro
 
 %macro add 2
  add %1, %2
-endmacro%
+%endmacro
 
 %macro subtract 2
   sub %1, %2
-endmacro%
+%endmacro
 
 %macro clear 
   xor %1, %1
